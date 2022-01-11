3b392495c254c4bff9984a602de2d72d8cbec152
kuhndev <whyyoutrybro@gmail.com>
2022-01-10 19:50:12 -0500

    added models, routes and server.js
    also updated required dependences

---
+++ routes/api.js
@@ -0,0 +1,51 @@
+const router = require("express").Router();
+const Workout = require("../models/Workout");
+
+// Server side routes
+router.get("/api/workouts", (req, res) => {
+
+    // find all workouts
+    Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+});
+router.post("/api/workouts", ({ body }, res) => {
+
+    // create new workout
+    Workout.create(body)
+        .then((dbWorkout => {
+            res.json(dbWorkout);
+        })).catch(err => {
+            res.json(err);
+        });
+});
+router.put("/api/workouts/:id", (req, res) => {
+
+    // update workout 
+    Workout.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+            $inc: { totalDuration: req.body.duration }, $push: { exercises: req.body }
+        },
+        { new: true })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+});
+router.get("/api/workouts/range", (req, res) => {
+
+    // find all workouts
+    Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+});
+
+module.exports = router;
